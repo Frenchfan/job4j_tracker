e851d036e54e7e8b9501eb1a39f88f162e6e7138
Frenchfan <sumkin.gpb@gmail.com>
2021-12-06 02:45:35 +0300

    1.6. Взаимодействие объектов [#166087]

--- src/main/java/ru/job4j/oop/Story.java
+++ src/main/java/ru/job4j/oop/Story.java
@@ -4,7 +4,7 @@ public class Story {
     public static void main(String[] args) {
         Pioneer petya = new Pioneer();
         Girl girl = new Girl();
-        Wolf wolf = new Wolf();
+        Wolf1 wolf = new Wolf1();
         girl.help(petya);
         wolf.eat(girl);
         petya.kill(wolf);

fed9c507e35b2a1891bb459144f89cb3cbe1dd0e
Frenchfan <sumkin.gpb@gmail.com>
2021-12-08 19:18:08 +0300

    2. Модель данных. [#174683]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -10,7 +10,7 @@ public class College {
         student.setFio("Petr Arsentev");
         student.setGroup(2);
         student.setAdmission(new Date(121, 2, 12));
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-YYYY");
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
         String formattedAdmission = formatter.format(student.getAdmission());
         System.out.println("Студент " + student.getFio() + ", группа " + student.getGroup() + ", зачислен "
                 + formattedAdmission);

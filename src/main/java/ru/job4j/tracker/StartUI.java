bea96caed96f5167ef33549f79585fc3312209de
Frenchfan <sumkin.gpb@gmail.com>
2021-12-08 04:17:59 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item oneItem = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String formattedDateTime = oneItem.getCreated().format(formatter);
+        System.out.println(formattedDateTime);
+    }
+}

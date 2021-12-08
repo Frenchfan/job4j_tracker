b39f027882baad33dd6c0680f0ca9124f82f27ab
Frenchfan <sumkin.gpb@gmail.com>
2021-12-08 19:17:29 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,18 @@
+package ru.job4j.pojo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) throws ParseException {
+        Student student = new Student();
+        student.setFio("Petr Arsentev");
+        student.setGroup(2);
+        student.setAdmission(new Date(121, 2, 12));
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-YYYY");
+        String formattedAdmission = formatter.format(student.getAdmission());
+        System.out.println("Студент " + student.getFio() + ", группа " + student.getGroup() + ", зачислен "
+                + formattedAdmission);
+    }
+}

4618278a44ba71aaea7df2ea28ebba68ec77b7fb
Frenchfan <sumkin.gpb@gmail.com>
2021-12-10 13:51:32 +0300

    3. Интерфейс Input [#181092 #237938]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,16 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
-
+        System.out.println("I go along the road");
     }
 
     @Override
     public void passengers(int countPassengers) {
-
+        System.out.println(countPassengers + " are aboard");
     }
 
     @Override
     public int refill(int litres) {
-        return 0;
+        return 100 - litres;
     }
 }

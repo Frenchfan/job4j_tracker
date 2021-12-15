c787b1cef0e5d257466d4097bf71faf93bc76f68
Frenchfan <sumkin.gpb@gmail.com>
2021-12-15 21:49:47 +0300

    2.1. Тест на ValidateInput [#311887]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -19,13 +19,13 @@ public class ValidateInput implements Input {
         boolean invalid = true;
         int value = -1;
         do {
-            String rsl = in.askStr(question);
-            if (!isNumber(rsl)) {
+            try {
+                value = in.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
                 System.out.println("Please enter valid data again.");
-                continue;
+
             }
-            value = Integer.parseInt(rsl);
-            invalid = false;
         } while (invalid);
         return value;
     }

415419147441f7c8959b39db7d4d02a094e95844
Frenchfan <sumkin.gpb@gmail.com>
2021-12-22 16:59:39 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #242829]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -1,9 +1,11 @@
 package ru.job4j.search;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 public class PhoneDictionaryTest {
@@ -15,5 +17,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenCouldNotFind() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Alexander");
+        assertThat(persons.isEmpty(), is(true));
     }
 }

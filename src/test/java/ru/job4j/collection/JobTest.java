e92ff0729ce0bdc0b3ac1695e41b056515a808d9
Frenchfan <sumkin.gpb@gmail.com>
2021-12-30 01:20:04 +0300

    2. Комбинированный компаратор [#10036 #245096]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -2,6 +2,8 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 import java.util.Comparator;
+
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
@@ -53,7 +55,7 @@ public class JobTest {
                 new Job("Fix bug", 0),
                 new Job("Fix bug", 1)
         );
-        assertThat(rsl, lessThan(0));
+        assertThat(rsl, greaterThan(0));
     }
 
     @Test

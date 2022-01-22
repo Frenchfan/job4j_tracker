7f91c4008f04d4485a086c6adb6e9693213273bc
Frenchfan <sumkin.gpb@gmail.com>
2022-01-22 23:24:14 +0300

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+    /**
+     *
+     * @param students список студентов
+     * @param predict условие фильтрации - можно указать в filter (он поддерживает предикаты)
+     * @return возвращает новую коллекцию
+     */
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
+    }
+}

f7458d4fb5ed2563695fab154de1d44fe7fe4d62
Frenchfan <sumkin.gpb@gmail.com>
2022-01-23 03:11:42 +0300

    4. Преобразование List в Map. [#110226]

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -6,7 +6,11 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ListToMap {
-
+    /**
+     * Метод преобразует лист в мапу, где фамилия - ключ, а student - значение
+     * @param list
+     * @return
+     */
     public static Map<String, Student> convert(List<Student> list) {
         return list.stream()
                 .distinct()

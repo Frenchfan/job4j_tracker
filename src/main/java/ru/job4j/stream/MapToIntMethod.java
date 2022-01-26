838b48629887bd56a6659d078d31d192c537ce1c
Frenchfan <sumkin.gpb@gmail.com>
2022-01-27 02:57:09 +0300

    5.5. Промежуточные операции. Метод mapToInt(). Сумма кодов символов

--- src/main/java/ru/job4j/stream/MapToIntMethod.java
+++ src/main/java/ru/job4j/stream/MapToIntMethod.java
@@ -3,6 +3,12 @@ package ru.job4j.stream;
 import java.util.List;
 
 public class MapToIntMethod {
+    /**
+     * converting the object stream to a primitive and summing up the codes of the characters
+     * pay attention to the (int) maptoInt method
+     * @param characters
+     * @return
+     */
     public static long sum(List<Character> characters) {
         return characters.stream()
                 .mapToInt(x -> (int) x)

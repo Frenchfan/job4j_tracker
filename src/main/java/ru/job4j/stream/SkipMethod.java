504e7cc924c5ebde7163f9315a465f7de941a594
Frenchfan <sumkin.gpb@gmail.com>
2022-01-27 17:41:17 +0300

    7.2. Промежуточные операции. Метод skip()

---
+++ src/main/java/ru/job4j/stream/SkipMethod.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * skip the first two elements
+ */
+public class SkipMethod {
+    public static List<String> lastThree(List<String> strings) {
+        return strings.stream()
+                .skip(2)
+                .collect(Collectors.toList());
+    }
+}
